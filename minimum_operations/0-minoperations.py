7d3bb4e0cee194c2a04a05e1aac65b23a2ab0094
sharangabo <e.sharangab@alustudent.com>
2023-05-19 22:51:43 +0200

    minimum operators

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+def minOperations(n):
+    """
+    Calculates the fewest number of operations needed to result in exactly n H characters.
+
+    Args:
+        n (int): The desired number of H characters.
+
+    Returns:
+        int: The minimum number of operations required.
+
+    """
+    if n <= 1:
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        if n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        else:
+            divisor += 1
+
+    return operations
+
+
+if __name__ == '__main__':
+    n = 4
+    print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+    n = 12
+    print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+
